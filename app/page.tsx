fafca0c16ac9f24cf29a24691784ea30355038c8
kyledyck@outlook.com <144828203+KyleJamesD@users.noreply.github.com>
2025-01-31 12:32:01 -0700

    fist commit basic react form completed no prisma or DB funcitons created yet

--- app/page.tsx
+++ app/page.tsx
@@ -1,95 +1,61 @@
+'use client'
+
 import Image from "next/image";
 import styles from "./page.module.css";
+import { useState } from "react";
 
 export default function Home() {
+
+
+  function handleSubmit () {
+    console.log ("handle submit pressed")
+  }
+
+const [studentId, setStudentId] = useState('');
+const [fullname, setFullName] = useState('');
+const [courseName, setCourseName] = useState('');
+const [date, setDate] = useState('');
+
+
+  
+
+
+
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+      <h1>Add New Student</h1>
+      <form onSubmit={handleSubmit} className={styles.page}>
+        <label>
+        <input type="text" 
+        name="username" 
+        placeholder="Student ID"
+        value={studentId} 
+        onChange={(event) =>(setStudentId(event.target.value))}></input>
+        </label>
+        <label>
+        <input type="text" 
+        name="username" 
+        placeholder="Full Name"
+        value={fullname} 
+        onChange={(event) =>(setFullName(event.target.value))}></input>
+        </label>
+        <label>
+        <input type="text" 
+        name="username" 
+        placeholder="Couse Name"
+        value={courseName} 
+        onChange={(event) =>(setCourseName(event.target.value))}></input>
+        </label>
+        <label>
+        <input type="text" 
+        name="username" 
+        placeholder="Date (dd/mm/yyyy)"
+        value={date} 
+        onChange={(event) =>(setDate(event.target.value))}></input>
+        </label>
+        <button className={styles.submitButton} type="submit">Add Student</button>
+      </form>
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
     </div>
   );
 }
